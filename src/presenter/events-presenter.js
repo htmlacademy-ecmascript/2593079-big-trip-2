04f9e0c62a88c720a807fa77770bc4c12eb01bf5
Daniil888-m <daniil.kolotygin80@gmail.com>
2025-02-28 22:44:57 +0300

    исправил проблему с линтером и обработчиком на нажатие escape

--- src/presenter/events-presenter.js
+++ src/presenter/events-presenter.js
@@ -21,42 +21,48 @@ export default class EventsPresenter {
     render(this.listComponent, this.eventsContainer);
 
     for (let i = 1; i < this.events.length; i++) {
-      const event = this.events[i];
-      const type = this.events[i].type;
-      const destination = this.eventsModel.getDestinationById(event.destination);
-      const offers = this.eventsModel.getOffersById(event.offers, type);
+      this.#createEvent(i);
+    }
+  }
 
-      const eventComponent = new EventView({
-        event, destination, offers, onClick: () => {
-          replaceEventToEditForm();
-          document.addEventListener('keydown', onEscKeyDown);
-        }
-      });
-      const editEventFormComponent = new EditEventView({
-        event, destination, offers,
-        allDestinations: this.eventsModel.getAllDestinationsNames(),
-        onSubmit: () => {
-          replaceEditFormToEvent();
-          document.removeEventListener('keydown', onEscKeyDown);
-        },
-        onClick: () => replaceEditFormToEvent()
-      });
+  #createEvent(index) {
+    const event = this.events[index];
+    const type = this.events[index].type;
+    const destination = this.eventsModel.getDestinationById(event.destination);
+    const offers = this.eventsModel.getOffersById(event.offers, type);
 
-      // Ругается на Function Declaration, хотя без поднятия здесь никак(есть ли какой-нибудь совет?)
-
-      function onEscKeyDown(event) {
-        event.preventDefault();
+    const eventComponent = new EventView({
+      event, destination, offers, onClick: () => {
+        replaceEventToEditForm();
+        document.addEventListener('keydown', onEscKeyDown);
+      }
+    });
+    const editEventFormComponent = new EditEventView({
+      event, destination, offers,
+      allDestinations: this.eventsModel.getAllDestinationsNames(),
+      onSubmit: () => {
+        replaceEditFormToEvent();
+        document.removeEventListener('keydown', onEscKeyDown);
+      },
+      onClick: () => {
         replaceEditFormToEvent();
         document.removeEventListener('keydown', onEscKeyDown);
       }
-      function replaceEventToEditForm() {
-        replace(editEventFormComponent, eventComponent);
-      };
-      function replaceEditFormToEvent() {
-        replace(eventComponent, editEventFormComponent);
-      };
-      render(eventComponent, this.listComponent.element);
+    });
 
+    // Ругается на Function Declaration, хотя без поднятия здесь никак(есть ли какой-нибудь совет?)
+
+    function onEscKeyDown(evt) {
+      evt.preventDefault();
+      replaceEditFormToEvent();
+      document.removeEventListener('keydown', onEscKeyDown);
     }
+    function replaceEventToEditForm() {
+      replace(editEventFormComponent, eventComponent);
+    }
+    function replaceEditFormToEvent() {
+      replace(eventComponent, editEventFormComponent);
+    }
+    render(eventComponent, this.listComponent.element);
   }
 }
