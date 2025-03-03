a3a2a9fd4646025f7f2c465e1304922c0b430524
Daniil888-m <daniil.kolotygin80@gmail.com>
2025-03-03 14:13:37 +0300

    исправил ошибки линтера

--- src/main.js
+++ src/main.js
@@ -1,14 +1,9 @@
-import { render } from './framework/render.js';
 import EventsModel from './model/events-model.js';
 import EventsPresenter from './presenter/events-presenter.js';
 
-
-const filtersContainer = document.querySelector('.trip-controls__filters');
 const eventsListContainer = document.querySelector('.trip-events');
 const eventsModel = new EventsModel();
 const eventsPresenter = new EventsPresenter({ eventsContainer: eventsListContainer, eventsModel });
-
-
 
 eventsPresenter.init();
 
